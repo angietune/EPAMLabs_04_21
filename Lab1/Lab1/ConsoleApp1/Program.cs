649360434953b58234b978a98371406597e4e138
angie <natalia@angie.local>
2021-04-21 20:29:47 +0300

    init: Lab1

--- Lab1/Lab1/ConsoleApp1/Program.cs
+++ Lab1/Lab1/ConsoleApp1/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
-
-namespace ConsoleApp1
+namespace array_sorting
 {
-    class Program
+    class array_sorting
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            // ввод чисел
+            int[] nums = new int[5];
+            Console.WriteLine("Введите пять чисел");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write("{0}-е число: ", i + 1);
+                nums[i] = Int32.Parse(Console.ReadLine());
+            }
+ 
+            // сортировка
+            int temp;
+            for (int i = 0; i < nums.Length-1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+             
+            // вывод
+            Console.WriteLine("Вывод отсортированного массива");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ Lab1/Lab1/CosoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace calc
+{
+    class calc
+    {
+        static void Main()
+        {
+            // ввод чисел
+            Console.WriteLine("Введите первое число");
+            int num1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int num2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Введите действие");
+            char sign = Convert.ToChar(Console.ReadLine());
+            int num = 0;
+
+            // вычисление
+            switch (sign)
+            {
+                case '+':
+                    num = num1 + num2; break;
+                case '-':
+                    num = num1 - num2; break;
+                case '*':
+                    num = num1 * num2; break;
+                case '/':
+                    num = num1 / num2; break;
+            }
+
+            // вывод
+            Console.WriteLine($"{num1} {sign} {num2} = {num}");
+        }
+    }
+}
