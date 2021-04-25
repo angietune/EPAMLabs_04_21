c2eb5c65104349a6888a2d253c0809c51e4d7f47
angie <natalia@angie.local>
2021-04-25 19:08:19 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EPAMLabs_04_21
+{
+    class Photobook {
+        public string Format { get; set; }
+        public int Pages { get; set; }
+
+        public Photobook (int pages) {
+            Pages = pages;
+        }
+        public Desciption (string format) {
+            Format = format;
+        }
+
+        public void Print () {
+            Console.WriteLine ("Format: " + Format, "Pages: " + Pages);
+        }
+    }
+    class Order : Photobook {
+        public int price;
+
+        public Order (int price, int pages) : base (pages) {
+            this.price = price * pages;
+        }
+    }
+    class MainClass {
+        public static void Main(string[] args)
+        {
+            Order book = new Order (10, 28);
+            Console.WriteLine("Price: " + price);
+
+            Photobook description = new Photobook ("20x20", 28);
+            description.Print();
+        }
+    }
+}
